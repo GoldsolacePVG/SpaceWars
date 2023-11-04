90a9d6a5bb37cfea6b7a9475d2ddfdd2f5bd8bb7
GoldsolacePVG <rodrigomicopliego@gmail.com>
2023-11-04 01:58:09 +0100

    Added correct player movement

--- Assets/Code/Player_Controller.cs
+++ Assets/Code/Player_Controller.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
 using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
     public Rigidbody2D player_rigid;
-    public float lateral_speed = 5.0f;
+    private float speed = 10.0f;
+
     void Start()
     {
         player_rigid = GetComponent<Rigidbody2D>();
@@ -13,11 +16,11 @@ public class Player_Controller : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
-            this.transform.position += Vector3.left * lateral_speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
-            this.transform.position += Vector3.right * lateral_speed * Time.deltaTime;
-        }
+        // PLAYER INPUTS
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) && this.transform.position.x > -6.23){this.transform.position += Vector3.left * speed * Time.deltaTime;}
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) && this.transform.position.x < 6.24){this.transform.position += Vector3.right * speed * Time.deltaTime;}
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {this.transform.position += Vector3.up * speed * Time.deltaTime;}
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) && this.transform.position.y > -5.99){this.transform.position += Vector3.down * speed * Time.deltaTime;}
+        if (Input.GetKey(KeyCode.Space)) {}
     }
 }
