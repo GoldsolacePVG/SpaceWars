195a1445110cb345a955d70feda335fa46768030
GoldsolacePVG <rodrigomicopliego@gmail.com>
2023-11-09 19:40:58 +0100

    Better exp

---
+++ Assets/Code/Stars_Flow.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stars_Flow : MonoBehaviour
+{
+    public GameObject[] planets;
+    public float timeBetweenPlanets;
+    public float planetsSpeed;
+    List<GameObject> planetsList = new List<GameObject>();
+    void Start() {
+        StartCoroutine(PlanetsCreation());
+    }
+
+    IEnumerator PlanetsCreation()
+    {
+        //Create a new list copying the arrey
+        for (int i = 0; i < planets.Length; i++)
+        {
+            planetsList.Add(planets[i]);
+        }
+        yield return new WaitForSeconds(10);
+        while (true)
+        {
+            ////choose random object from the list, generate and delete it
+            int randomIndex = Random.Range(0, planetsList.Count);
+            GameObject newPlanet = Instantiate(planetsList[randomIndex]);
+            planetsList.RemoveAt(randomIndex);
+            //if the list decreased to zero, reinstall it
+            if (planetsList.Count == 0)
+            {
+                for (int i = 0; i < planets.Length; i++)
+                {
+                    planetsList.Add(planets[i]);
+                }
+            }
+            newPlanet.GetComponent<DirectMoving>().speed = planetsSpeed;
+
+            yield return new WaitForSeconds(timeBetweenPlanets);
+        }
+    }
+}
